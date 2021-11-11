41cb4503aafd29e0897f36c2f719d74a35ab53d3
Sean <craig.sean92@gmail.com>
2021-11-11 13:57:27 -0700

    Completed Assignment

--- main.js
+++ main.js
@@ -2,9 +2,41 @@
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
+
 // Your JavaScript code goes here!
+document.addEventListener("DOMContentLoaded", () => {
+  const modal = document.querySelector('#modal')
+  const hearts = document.getElementsByClassName('like-glyph')
+  likePost(hearts)
+})
 
+const likePost = (hearts) => {
+  for (const heart of hearts){
+    heart.addEventListener("click", (e) => {
+    
+    mimicServerCall()
+    .then(() => {
+      if(heart.innerHTML == EMPTY_HEART) {
+      heart.innerHTML = FULL_HEART
+      heart.className= "activated-heart"
+      } else {
+        heart.innerHTML = EMPTY_HEART
+        heart.className = 'like-glyph'
+      }
+    })
+    .catch(error => {
+      modal.hidden = false
+      const modalMessage = document.querySelector("#modal-message")
+      modalMessage.innerText = error
+      setTimeout(() => {
+        modal.hidden = true
+      }, 5000
+      )
+    })
+    })
+  }
 
+}
 
 
 //------------------------------------------------------------------------------
